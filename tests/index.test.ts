d2e73883494f7bc2dccc0b1b10eab7a3b49ae65d
Lewis Thorley <lewis@beacon.co.uk>
2022-01-10 14:56:53 +0000

    Update e2e script

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -4,6 +4,6 @@ test('basic test', async ({ page }) => {
   await page.goto('/planet/1');
   const title = page.locator('#title');
   await expect(title).toHaveText('Planet Information');
-  const element = await page.waitForSelector('text=Tatooine', { timeout: 5000 });
+  const element = await page.waitForSelector('"Tatooine"');
   expect(element).toBeDefined();
 });
